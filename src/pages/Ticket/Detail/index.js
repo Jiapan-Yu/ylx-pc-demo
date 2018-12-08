1a46a7d9d36fac2ee81b4104ece85ac624c353e0
Yu Jiapan <2541771811yjp@gmail.com>
2018-12-08 21:02:29 +0800

    门票详情页info完成部分

--- src/pages/Ticket/Detail/index.js
+++ src/pages/Ticket/Detail/index.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react'
 import './index.less'
 
+const infoList = ['预订须知', '景点介绍', '交通信息']
 class TicketDetail extends Component {
 	state = {}
+
+	componentDidMount() {
+		window.addEventListener('scroll', this.scrollHandler)
+	}
+
+	scrollHandler = (e) => {
+		console.log(e)
+	}
+
+	componentWillUnmount() {
+		window.removeEventListener('scroll', this.scrollHandler)
+	}
+
 	render() {
 		return (
 			<div className="ticket-detail-wrap">
@@ -49,6 +63,175 @@ lacinia nulla vitae enim. Pellentesque tincidunt purus vel magna. Integer non en
 euismod nunc eu purus. Donec bibendum quam in tellus. Nullam cursus pulvinar lectus. Donec
 et mi. Nam vulputate metus eu enim. Vestibulum pellentesque felis eu massa.
 				</div>
+
+				<div id="infolist" className="info">
+					{infoList.map((value, i) => {
+						return <span key={i} className={`item ${i}`}>{value}</span>
+					})}
+				</div>
+				<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Ut purus elit, vestibulum ut,
+	placerat ac, adipiscing vitae, felis. Curabitur dictum gravida mauris. Nam arcu libero, non-
+	ummy eget, consectetuer id, vulputate a, magna. Donec vehicula augue eu neque. Pellentesque
+	habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Mauris ut leo.
+	Cras viverra metus rhoncus sem. Nulla et lectus vestibulum urna fringilla ultrices. Phasellus
+	eu tellus sit amet tortor gravida placerat. Integer sapien est, iaculis in, pretium quis, viverra
+	ac, nunc. Praesent eget sem vel leo ultrices bibendum. Aenean faucibus. Morbi dolor nulla,
+	malesuada eu, pulvinar at, mollis ac, nulla. Curabitur auctor semper nulla. Donec varius orci
+	eget risus. Duis nibh mi, congue eu, accumsan eleifend, sagittis quis, diam. Duis eget orci sit
+	amet orci dignissim rutrum.<br /><br />
+
+					Nam dui ligula, fringilla a, euismod sodales, sollicitudin vel, wisi. Morbi auctor lorem non
+				justo. Nam lacus libero, pretium at, lobortis vitae, ultricies et, tellus. Donec aliquet, tortor sed
+				accumsan bibendum, erat ligula aliquet magna, vitae ornare odio metus a mi. Morbi ac orci et
+				nisl hendrerit mollis. Suspendisse ut massa. Cras nec ante. Pellentesque a nulla. Cum sociis
+				natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam tincidunt
+urna. Nulla ullamcorper vestibulum turpis. Pellentesque cursus luctus mauris.<br /><br />
+
+					Nulla malesuada porttitor diam. Donec felis erat, congue non, volutpat at, tincidunt tris-
+					tique, libero. Vivamus viverra fermentum felis. Donec nonummy pellentesque ante. Phasellus
+					adipiscing semper elit. Proin fermentum massa ac quam. Sed diam turpis, molestie vitae,
+					placerat a, molestie nec, leo. Maecenas lacinia. Nam ipsum ligula, eleifend at, accumsan nec,
+					suscipit a, ipsum. Morbi blandit ligula feugiat magna. Nunc eleifend consequat lorem. Sed
+					lacinia nulla vitae enim. Pellentesque tincidunt purus vel magna. Integer non enim. Praesent
+					euismod nunc eu purus. Donec bibendum quam in tellus. Nullam cursus pulvinar lectus. Donec
+et mi. Nam vulputate metus eu enim. Vestibulum pellentesque felis eu massa.<br /><br />
+
+					Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Ut purus elit, vestibulum ut,
+						placerat ac, adipiscing vitae, felis. Curabitur dictum gravida mauris. Nam arcu libero, non-
+						ummy eget, consectetuer id, vulputate a, magna. Donec vehicula augue eu neque. Pellentesque
+						habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Mauris ut leo.
+						Cras viverra metus rhoncus sem. Nulla et lectus vestibulum urna fringilla ultrices. Phasellus
+						eu tellus sit amet tortor gravida placerat. Integer sapien est, iaculis in, pretium quis, viverra
+						ac, nunc. Praesent eget sem vel leo ultrices bibendum. Aenean faucibus. Morbi dolor nulla,
+						malesuada eu, pulvinar at, mollis ac, nulla. Curabitur auctor semper nulla. Donec varius orci
+						eget risus. Duis nibh mi, congue eu, accumsan eleifend, sagittis quis, diam. Duis eget orci sit
+	amet orci dignissim rutrum.<br /><br />
+
+					Nam dui ligula, fringilla a, euismod sodales, sollicitudin vel, wisi. Morbi auctor lorem non
+				justo. Nam lacus libero, pretium at, lobortis vitae, ultricies et, tellus. Donec aliquet, tortor sed
+				accumsan bibendum, erat ligula aliquet magna, vitae ornare odio metus a mi. Morbi ac orci et
+				nisl hendrerit mollis. Suspendisse ut massa. Cras nec ante. Pellentesque a nulla. Cum sociis
+				natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam tincidunt
+urna. Nulla ullamcorper vestibulum turpis. Pellentesque cursus luctus mauris.<br /><br />
+
+					Nulla malesuada porttitor diam. Donec felis erat, congue non, volutpat at, tincidunt tris-
+					tique, libero. Vivamus viverra fermentum felis. Donec nonummy pellentesque ante. Phasellus
+					adipiscing semper elit. Proin fermentum massa ac quam. Sed diam turpis, molestie vitae,
+					placerat a, molestie nec, leo. Maecenas lacinia. Nam ipsum ligula, eleifend at, accumsan nec,
+					suscipit a, ipsum. Morbi blandit ligula feugiat magna. Nunc eleifend consequat lorem. Sed
+					lacinia nulla vitae enim. Pellentesque tincidunt purus vel magna. Integer non enim. Praesent
+					euismod nunc eu purus. Donec bibendum quam in tellus. Nullam cursus pulvinar lectus. Donec
+et mi. Nam vulputate metus eu enim. Vestibulum pellentesque felis eu massa.<br /><br />
+
+					景点介绍<br /><br />
+					Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Ut purus elit, vestibulum ut,
+						placerat ac, adipiscing vitae, felis. Curabitur dictum gravida mauris. Nam arcu libero, non-
+						ummy eget, consectetuer id, vulputate a, magna. Donec vehicula augue eu neque. Pellentesque
+						habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Mauris ut leo.
+						Cras viverra metus rhoncus sem. Nulla et lectus vestibulum urna fringilla ultrices. Phasellus
+						eu tellus sit amet tortor gravida placerat. Integer sapien est, iaculis in, pretium quis, viverra
+						ac, nunc. Praesent eget sem vel leo ultrices bibendum. Aenean faucibus. Morbi dolor nulla,
+						malesuada eu, pulvinar at, mollis ac, nulla. Curabitur auctor semper nulla. Donec varius orci
+						eget risus. Duis nibh mi, congue eu, accumsan eleifend, sagittis quis, diam. Duis eget orci sit
+	amet orci dignissim rutrum.<br /><br />
+
+					Nam dui ligula, fringilla a, euismod sodales, sollicitudin vel, wisi. Morbi auctor lorem non
+				justo. Nam lacus libero, pretium at, lobortis vitae, ultricies et, tellus. Donec aliquet, tortor sed
+				accumsan bibendum, erat ligula aliquet magna, vitae ornare odio metus a mi. Morbi ac orci et
+				nisl hendrerit mollis. Suspendisse ut massa. Cras nec ante. Pellentesque a nulla. Cum sociis
+				natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam tincidunt
+urna. Nulla ullamcorper vestibulum turpis. Pellentesque cursus luctus mauris.<br /><br />
+
+					Nulla malesuada porttitor diam. Donec felis erat, congue non, volutpat at, tincidunt tris-
+					tique, libero. Vivamus viverra fermentum felis. Donec nonummy pellentesque ante. Phasellus
+					adipiscing semper elit. Proin fermentum massa ac quam. Sed diam turpis, molestie vitae,
+					placerat a, molestie nec, leo. Maecenas lacinia. Nam ipsum ligula, eleifend at, accumsan nec,
+					suscipit a, ipsum. Morbi blandit ligula feugiat magna. Nunc eleifend consequat lorem. Sed
+					lacinia nulla vitae enim. Pellentesque tincidunt purus vel magna. Integer non enim. Praesent
+					euismod nunc eu purus. Donec bibendum quam in tellus. Nullam cursus pulvinar lectus. Donec
+et mi. Nam vulputate metus eu enim. Vestibulum pellentesque felis eu massa.<br /><br />
+
+					Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Ut purus elit, vestibulum ut,
+						placerat ac, adipiscing vitae, felis. Curabitur dictum gravida mauris. Nam arcu libero, non-
+						ummy eget, consectetuer id, vulputate a, magna. Donec vehicula augue eu neque. Pellentesque
+						habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Mauris ut leo.
+						Cras viverra metus rhoncus sem. Nulla et lectus vestibulum urna fringilla ultrices. Phasellus
+						eu tellus sit amet tortor gravida placerat. Integer sapien est, iaculis in, pretium quis, viverra
+						ac, nunc. Praesent eget sem vel leo ultrices bibendum. Aenean faucibus. Morbi dolor nulla,
+						malesuada eu, pulvinar at, mollis ac, nulla. Curabitur auctor semper nulla. Donec varius orci
+						eget risus. Duis nibh mi, congue eu, accumsan eleifend, sagittis quis, diam. Duis eget orci sit
+	amet orci dignissim rutrum.<br /><br />
+
+					Nam dui ligula, fringilla a, euismod sodales, sollicitudin vel, wisi. Morbi auctor lorem non
+				justo. Nam lacus libero, pretium at, lobortis vitae, ultricies et, tellus. Donec aliquet, tortor sed
+				accumsan bibendum, erat ligula aliquet magna, vitae ornare odio metus a mi. Morbi ac orci et
+				nisl hendrerit mollis. Suspendisse ut massa. Cras nec ante. Pellentesque a nulla. Cum sociis
+				natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam tincidunt
+urna. Nulla ullamcorper vestibulum turpis. Pellentesque cursus luctus mauris.<br /><br />
+
+					Nulla malesuada porttitor diam. Donec felis erat, congue non, volutpat at, tincidunt tris-
+					tique, libero. Vivamus viverra fermentum felis. Donec nonummy pellentesque ante. Phasellus
+					adipiscing semper elit. Proin fermentum massa ac quam. Sed diam turpis, molestie vitae,
+					placerat a, molestie nec, leo. Maecenas lacinia. Nam ipsum ligula, eleifend at, accumsan nec,
+					suscipit a, ipsum. Morbi blandit ligula feugiat magna. Nunc eleifend consequat lorem. Sed
+					lacinia nulla vitae enim. Pellentesque tincidunt purus vel magna. Integer non enim. Praesent
+					euismod nunc eu purus. Donec bibendum quam in tellus. Nullam cursus pulvinar lectus. Donec
+et mi. Nam vulputate metus eu enim. Vestibulum pellentesque felis eu massa.<br /><br />
+
+					交通信息<br /><br />
+					Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Ut purus elit, vestibulum ut,
+						placerat ac, adipiscing vitae, felis. Curabitur dictum gravida mauris. Nam arcu libero, non-
+						ummy eget, consectetuer id, vulputate a, magna. Donec vehicula augue eu neque. Pellentesque
+						habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Mauris ut leo.
+						Cras viverra metus rhoncus sem. Nulla et lectus vestibulum urna fringilla ultrices. Phasellus
+						eu tellus sit amet tortor gravida placerat. Integer sapien est, iaculis in, pretium quis, viverra
+						ac, nunc. Praesent eget sem vel leo ultrices bibendum. Aenean faucibus. Morbi dolor nulla,
+						malesuada eu, pulvinar at, mollis ac, nulla. Curabitur auctor semper nulla. Donec varius orci
+						eget risus. Duis nibh mi, congue eu, accumsan eleifend, sagittis quis, diam. Duis eget orci sit
+	amet orci dignissim rutrum.<br /><br />
+
+					Nam dui ligula, fringilla a, euismod sodales, sollicitudin vel, wisi. Morbi auctor lorem non
+				justo. Nam lacus libero, pretium at, lobortis vitae, ultricies et, tellus. Donec aliquet, tortor sed
+				accumsan bibendum, erat ligula aliquet magna, vitae ornare odio metus a mi. Morbi ac orci et
+				nisl hendrerit mollis. Suspendisse ut massa. Cras nec ante. Pellentesque a nulla. Cum sociis
+				natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam tincidunt
+urna. Nulla ullamcorper vestibulum turpis. Pellentesque cursus luctus mauris.<br /><br />
+
+					Nulla malesuada porttitor diam. Donec felis erat, congue non, volutpat at, tincidunt tris-
+					tique, libero. Vivamus viverra fermentum felis. Donec nonummy pellentesque ante. Phasellus
+					adipiscing semper elit. Proin fermentum massa ac quam. Sed diam turpis, molestie vitae,
+					placerat a, molestie nec, leo. Maecenas lacinia. Nam ipsum ligula, eleifend at, accumsan nec,
+					suscipit a, ipsum. Morbi blandit ligula feugiat magna. Nunc eleifend consequat lorem. Sed
+					lacinia nulla vitae enim. Pellentesque tincidunt purus vel magna. Integer non enim. Praesent
+					euismod nunc eu purus. Donec bibendum quam in tellus. Nullam cursus pulvinar lectus. Donec
+et mi. Nam vulputate metus eu enim. Vestibulum pellentesque felis eu massa.<br /><br />
+
+					Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Ut purus elit, vestibulum ut,
+						placerat ac, adipiscing vitae, felis. Curabitur dictum gravida mauris. Nam arcu libero, non-
+						ummy eget, consectetuer id, vulputate a, magna. Donec vehicula augue eu neque. Pellentesque
+						habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Mauris ut leo.
+						Cras viverra metus rhoncus sem. Nulla et lectus vestibulum urna fringilla ultrices. Phasellus
+						eu tellus sit amet tortor gravida placerat. Integer sapien est, iaculis in, pretium quis, viverra
+						ac, nunc. Praesent eget sem vel leo ultrices bibendum. Aenean faucibus. Morbi dolor nulla,
+						malesuada eu, pulvinar at, mollis ac, nulla. Curabitur auctor semper nulla. Donec varius orci
+						eget risus. Duis nibh mi, congue eu, accumsan eleifend, sagittis quis, diam. Duis eget orci sit
+	amet orci dignissim rutrum.<br /><br />
+
+					Nam dui ligula, fringilla a, euismod sodales, sollicitudin vel, wisi. Morbi auctor lorem non
+				justo. Nam lacus libero, pretium at, lobortis vitae, ultricies et, tellus. Donec aliquet, tortor sed
+				accumsan bibendum, erat ligula aliquet magna, vitae ornare odio metus a mi. Morbi ac orci et
+				nisl hendrerit mollis. Suspendisse ut massa. Cras nec ante. Pellentesque a nulla. Cum sociis
+				natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam tincidunt
+urna. Nulla ullamcorper vestibulum turpis. Pellentesque cursus luctus mauris.<br /><br />
+
+					Nulla malesuada porttitor diam. Donec felis erat, congue non, volutpat at, tincidunt tris-
+					tique, libero. Vivamus viverra fermentum felis. Donec nonummy pellentesque ante. Phasellus
+					adipiscing semper elit. Proin fermentum massa ac quam. Sed diam turpis, molestie vitae,
+					placerat a, molestie nec, leo. Maecenas lacinia. Nam ipsum ligula, eleifend at, accumsan nec,
+					suscipit a, ipsum. Morbi blandit ligula feugiat magna. Nunc eleifend consequat lorem. Sed
+					lacinia nulla vitae enim. Pellentesque tincidunt purus vel magna. Integer non enim. Praesent
+					euismod nunc eu purus. Donec bibendum quam in tellus. Nullam cursus pulvinar lectus. Donec
+et mi. Nam vulputate metus eu enim. Vestibulum pellentesque felis eu massa.</p>
 			</div>
 		)
 	}
