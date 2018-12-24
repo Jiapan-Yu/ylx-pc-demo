28aa07084cc4b66f32ff49fcc1705b662cd39214
Yu Jiapan <2541771811yjp@gmail.com>
2018-12-24 13:21:55 +0800

    ant design grid part 栅格配置器

--- src/pages/Grid/index.js
+++ src/pages/Grid/index.js
@@ -1,17 +1,71 @@
 import React, { Component } from 'react'
 import './index.less'
-import { Row, Col } from 'antd'
+import { Row, Col, Slider } from 'antd'
 
 class Grid extends Component {
+	gutters = {}
+
+	colCounts = {}
+
+	constructor() {
+		super()
+		this.state = {
+			gutterKey: 1,
+			colCountKey: 2
+		};  // don't know why these two places need to end with semi-colon
+		[8, 16, 24, 32, 40, 48].forEach((value, i) => { this.gutters[i] = value; });  // don't know why these two places need to end with semi-colon
+		[2, 3, 4, 6, 8, 12].forEach((value, i) => { this.colCounts[i] = value; })
+	}
+
+	onGutterChange = (gutterKey) => {
+		this.setState({ gutterKey })
+	}
+
+	onColCountChange = (colCountKey) => {
+		this.setState({ colCountKey })
+	}
+
 	render() {
+		const { gutterKey, colCountKey } = this.state
+		const cols = []
+		const colCount = this.colCounts[colCountKey]
+		let colCode = ''
+		for (let i = 0; i < colCount; i++) {
+			cols.push(
+				<Col key={i.toString()} span={24 / colCount}>
+					<div>Column</div>
+				</Col>
+			)
+			colCode += ` <Col span={${24 / colCount}} />\n`
+		}
 		return (
 			<div>
-				<Row type="flex">
-					<Col span={6} order={4}>1 col-order-4</Col>
-					<Col span={6} order={3}>2 col-order-3</Col>
-					<Col span={6} order={2}>3 col-order-2</Col>
-					<Col span={6} order={1}>4 col-order-1</Col>
-				</Row>
+				<div style={{ marginBottom: 16 }}>
+					<span style={{ marginRight: 6 }}>Gutter (px): </span>
+					<div style={{ width: '50%' }}>
+						<Slider
+							min={0}
+							max={Object.keys(this.gutters).length - 1}
+							value={gutterKey}
+							onChange={this.onGutterChange}
+							marks={this.gutters}
+							step={null}
+						/>
+					</div>
+					<span style={{ marginRight: 6 }}>Column Count:</span>
+					<div style={{ width: '50%' }}>
+						<Slider
+							min={0}
+							max={Object.keys(this.colCounts).length - 1}
+							value={colCountKey}
+							onChange={this.onColCountChange}
+							marks={this.colCounts}
+							step={null}
+						/>
+					</div>
+				</div>
+				<Row gutter={this.gutters[gutterKey]}>{cols}</Row>
+				<pre>{`<Row gutter={${this.gutters[gutterKey]}}>\n${colCode}</Row>`}</pre>
 			</div>
 		)
 	}
