dda989293bd442d20b2d742e81c0d0e6a373509e
Yu Jiapan <2541771811yjp@gmail.com>
2018-12-24 10:06:21 +0800

    ant design grid part flex排序

--- src/pages/Grid/index.js
+++ src/pages/Grid/index.js
@@ -6,12 +6,11 @@ class Grid extends Component {
 	render() {
 		return (
 			<div>
-				<p>sub-element align</p>
-				<Row type="flex" justify="space-around">
-					<Col span={4}>col-4</Col>
-					<Col span={4}>col-4</Col>
-					<Col span={4}>col-4</Col>
-					<Col span={4}>col-4</Col>
+				<Row type="flex">
+					<Col span={6} order={4}>1 col-order-4</Col>
+					<Col span={6} order={3}>2 col-order-3</Col>
+					<Col span={6} order={2}>3 col-order-2</Col>
+					<Col span={6} order={1}>4 col-order-1</Col>
 				</Row>
 			</div>
 		)
